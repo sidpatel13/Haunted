8cdc4054173152be697f40ea0bc3b95abbfd5229
juljun14 <juljun14@gmail.com>
2014-08-14 18:47:33 -0400

    delete chat controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  # devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   
+  get '/rooms', to: 'rooms#generate'
+  get '/:room_session', to: 'rooms#show'
 end
