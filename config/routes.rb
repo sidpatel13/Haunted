bb4dacff3b9afa06f1de086b69b7869f5b16e00d
juljun14 <juljun14@gmail.com>
2014-08-14 18:53:11 -0400

    create routes for home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  # devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
-  
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+  root 'home#index'
   get '/rooms', to: 'rooms#generate'
   get '/:room_session', to: 'rooms#show'
 end
