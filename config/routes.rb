9be616702cb3e0f2d353af251c1b718f788b0753
juljun14 <juljun14@gmail.com>
2014-08-14 18:29:01 -0400

    delete pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
-  get '/pages/home', to: 'pages#home'
-  get '/pages/game', to: 'pages#game'
-  get '/pages/chat', to: 'pages#chat'
   
 end
