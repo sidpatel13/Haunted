40591ab6024f0a144cb6bfa0089b3f736c7bcaa2
Sid Patel <sidpatel13@gmail.com>
2014-08-14 23:12:52 -0400

    Add enable condition to control permission for motion.

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -9,6 +9,7 @@ function preload() {
 
 var person;
 var ghost1; var ghost2; var ghost3; var ghost4;
+var ghosts;
 var key1; var key2; var key3; var key4;
 var walls;
 
@@ -90,18 +91,23 @@ function create() {
     person.scale.setTo(0.5, 0.5);
     person.anchor.setTo(0.5, 0.5);
 
-    // Create ghost 1.
+    // Create ghosts.
 
     ghost1 = game.add.sprite(100, game.world.height - 150, 'ghost');
     ghost1.anchor.setTo(0.5, 0.5);
     ghost1.scale.setTo(2,2);
+
+    ghost2 = game.add.sprite(200, game.world.height - 150, 'ghost');
+    ghost2.anchor.setTo(0.5, 0.5);
+    ghost2.scale.setTo(2,2);
+
+    ghosts = game.add.group(ghost1, ghost2);
 
     //  Enable physics for sprites, make world boundaries.
 
     game.physics.arcade.enable([person, ghost1]);
     person.body.collideWorldBounds = true;
     ghost1.body.collideWorldBounds = true;
-
 
     // Create hotkeys.
 
@@ -110,24 +116,58 @@ function create() {
     key3 = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
     key4 = game.input.keyboard.addKey(Phaser.Keyboard.FOUR);
 
-    key1.onDown.add(function() { console.log("testing key1") });
-    key2.onDown.add(function() { console.log("testing key2") });
-    key3.onDown.add(function() { console.log("testing key3") });
-    key4.onDown.add(function() { console.log("testing key4") });
+    key1.onDown.add(function() { makeActive(1) } );
+    key2.onDown.add(function() { makeActive(2) } );
+    key3.onDown.add(function() { makeActive(3) } );
+    key4.onDown.add(function() { makeActive(4) } );
+
+    //this.game.input.onDownCallback = function(e) { console.log('more tests')};
+
+    // this.game.input.keyboard.onDownCallback = function(e) {
+    //   if (e.keyCode == 49)
+    //     console.log("this is key 1");
+    //   else if (e.keyCode == 50)
+    //     console.log("this is key 2");
+    //   else if (e.keyCode == 51)
+    //     console.log("this is key 3");
+    //   else (e.keyCode == 52)
+    //     console.log("this is key 4");
+    //   else
+    //     console.log("not a hot key");
+    // };
 }
 
-// function makeActive() {
-//   if (person.body.enable) {
+// Make one ghost active and the others inactive.
 
-// }
+function makeActive(hotkey) {
+  console.log("You've selected " + hotkey)
+  for (var i = 1; i <= 4; i++) {
+    if (i === hotkey) {
+      person.body.enable = false;
+    }
+    else {
+      console.log("You need to disable these.")
+      // .body.enable = false;
+    }
+  };
+}
 
 function update() {
 
-  if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) { person.x -= 4;}
-  else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) { person.x += 4; }
-
-  if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) { person.y -= 4; }
-  else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) { person.y +=4; }
+  if (person.body.enable == true) {
+    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+      person.x -= 4;
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+      person.x += 4;
+    }
+    if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+      person.y -= 4;
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+      person.y +=4;
+    }
+  }
 
 
 }
