fe0271cafc400034d7b07b8f2c57a5175e9c75fb
Cassie Moy <moy.cassie@gmail.com>
2014-08-15 01:23:17 -0400

    Create function to make hotkey-selected ghost active.

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -9,25 +9,13 @@ function preload() {
 
 var person;
 var ghost1; var ghost2; var ghost3; var ghost4;
+var ghosts;
 var key1; var key2; var key3; var key4;
 var walls;
 
 function create() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    //  Create group for walls and enable physics
-    
-    // walls = game.add.group();
-    // walls.enableBody = true;
-    
-    // Create person.
-
-    person = game.add.sprite(1, game.world.height - 150, 'person');
-    person.scale.setTo(0.5,0.5);
-    person.anchor.setTo(0.5, 0.5);
-
-    // Create ghost 1.
 
     platforms = game.add.group();
 
@@ -97,49 +85,87 @@ function create() {
 
     }
 
-    // Here we create the ground.
-    // var ground = walls.create(0, game.world.height - 64, 'ground');
+    // Create person.
+
+    person = game.add.sprite(1, game.world.height - 150, 'person');
+    person.scale.setTo(0.5, 0.5);
+    person.anchor.setTo(0.5, 0.5);
+
+    // Create ghosts.
 
     ghost1 = game.add.sprite(100, game.world.height - 150, 'ghost');
     ghost1.anchor.setTo(0.5, 0.5);
     ghost1.scale.setTo(2,2);
+    
+    ghost2 = game.add.sprite(200, game.world.height - 150, 'ghost');
+    ghost2.anchor.setTo(0.5, 0.5);
+    ghost2.scale.setTo(2,2)
+
+    ghosts = [ghost1, ghost2];
+
+    //  Enable physics for sprites, make world boundaries.
+
+    game.physics.arcade.enable([person, ghost1, ghost2]);
+    person.body.collideWorldBounds = true;
+    ghost1.body.collideWorldBounds = true;
+    ghost2.body.collideWorldBounds = true;
+
+    // Make ghosts inactive by default.
+    ghost1.body.enable = false;
+    ghost2.body.enable = false;
 
     // Create hotkeys.
 
-    // key1 = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
-    // key2 = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
-    // key3 = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
+    key1 = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+    key2 = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
+    key3 = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
+    key4 = game.input.keyboard.addKey(Phaser.Keyboard.FOUR);
 
-    //  Enable physics for sprites, make world boundaries.
-    
-    game.physics.arcade.enable([person, ghost1]);
-    person.body.collideWorldBounds = true;
-    ghost1.body.collideWorldBounds = true;
-
-    // Toggle between characters
+    key1.onDown.add(function() { makeActive(1) } );
+    key2.onDown.add(function() { makeActive(2) } );
+    key3.onDown.add(function() { makeActive(3) } );
+    key4.onDown.add(function() { makeActive(4) } );
 }
-
-// function toggleBody() {
-//   if (person.body.enable) {
-  
-// }
 
 function update() {
 
-  if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-    person.x -= 4;
-  }
-  else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-    person.x += 4;
+  if (person.body.enable == true) {
+    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+      person.x -= 4;
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+      person.x += 4;
+    }
+    if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+      person.y -= 4;
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+      person.y +=4;
+    }
   }
 
-  if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-    person.y -= 4;
-  }
-  else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-    person.y +=4;
-  }
-
-  //game.physics.arcade.collide(ghost1, person);
+  ghosts.forEach(function(item) {
+      if (item.body.enable == true) {
+        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+          item.x -= 4;
+        }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+          item.x += 4;
+        }
+        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+          item.y -= 4;
+        }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+          item.y +=4;
+        }
+      }
+  });
 
 }
+
+
+
+function makeActive(hotkey) {
+  console.log(hotkey)
+  ghosts[(hotkey - 1)].body.enable = true;  
+};
