80b45b4d65de146df9bfce5f3aa7c2ae05d6afd1
Cassie Moy <moy.cassie@gmail.com>
2014-08-17 16:35:39 -0400

    Commit before pulling.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-    $("#fade").hide(0).delay(300).fadeIn(3000)
+    $("#fade").hide(0).delay(300).fadeIn(3000);
 });
 
-$("#welcome.sign_up_sign_in").click(function(e) {
-  e.preventDefault();
-  $("#")
-}
