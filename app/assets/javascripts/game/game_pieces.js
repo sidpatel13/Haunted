086ecc99ba91e355584e81c2b03bc14e8381544e
Julius Jung & Sid Patel <pair+juljun14.sidpatel13@devbootcamp.com>
2014-08-17 22:52:53 -0400

    moved game pieces that didn't deal with board into separate file and made it more OO by creating object literals

--- app/assets/javascripts/game/game_pieces.js
+++ app/assets/javascripts/game/game_pieces.js
@@ -1,52 +1,23 @@
-// var character = {};
-// character.createPerson
+gamePieces = {};
 
-var gamePieces = {};
+gamePieces.createTeleport = function() {
+  starOne = game.add.sprite(10, game.world.height - 300, 'star');
+  starOne.anchor.setTo(0.5, 0.5);
+  starOne.scale.setTo(1,1);
 
-gamePieces.createPerson = function() {
-  person = game.add.sprite(100, game.world.height - 150, 'person');
-  person.scale.setTo(0.5, 0.5);
-  person.anchor.setTo(0.5, 0.5);
-  person.userControl = true;
-  characters.push(person);
+  starTwo = game.add.sprite(790, game.world.height - 300, 'star');
+  starTwo.anchor.setTo(0.5, 0.5);
+  starTwo.scale.setTo(1,1);
 }
 
-gamePieces.createGhosts = function() {
+gamePieces.createOneDot = function() {
+    dot = game.add.sprite(Math.random()*800, Math.random()*600, 'diamond');
+    dot.anchor.setTo(0.5, 0.5);
+    dots.push(dot);
+};
 
-  //ghosts = game.add.group();
-  //ghosts.enableBody = true;
-
-  ghost1 = game.add.sprite(100, game.world.height - 100, 'ghost');
-  ghost1.anchor.setTo(0.5, 0.5);
-  ghost1.scale.setTo(2,2);
-  ghost1.isControlled = true;
-
-  ghost2 = game.add.sprite(200, game.world.height - 150, 'ghost');
-  ghost2.anchor.setTo(0.5, 0.5);
-  ghost2.scale.setTo(2,2);
-  ghost2.isControlled = true;
-
-  ghost3 = game.add.sprite(300, game.world.height - 150, 'ghost');
-  ghost3.anchor.setTo(0.5, 0.5);
-  ghost3.scale.setTo(2,2);
-  ghost3.isControlled = true;
-
-  ghost4 = game.add.sprite(400, game.world.height - 150, 'ghost');
-  ghost4.anchor.setTo(0.5, 0.5);
-  ghost4.scale.setTo(2,2);
-  ghost4.isControlled = true;
-
-  ghosts.push(ghost1, ghost2, ghost3, ghost4);
-  characters.push(ghost1, ghost2, ghost3, ghost4);
-}
-
-
-gamePieces.setUserControl = function(ghosts, ghostNumber) {
-  for(var i = 0; i < ghosts.length; i++) {
-    if (i === (ghostNumber -1)) {
-      ghosts[i].userControl = true;
-    } else {
-      ghosts[i].userControl = false;
-    }
+gamePieces.createMultipleDots = function(count) {
+    for(var i = 0; i < count; i++){
+    gamePieces.createOneDot();
   }
-}
+};
