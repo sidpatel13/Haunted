545a5db8ae0ca6f7c1c0721ea3d88ca4290e5007
Julius Jung & Sid Patel <pair+juljun14.sidpatel13@devbootcamp.com>
2014-08-18 00:29:06 -0400

    create keys array and push each key into the array

--- app/assets/javascripts/game/controls.js
+++ app/assets/javascripts/game/controls.js
@@ -5,4 +5,6 @@ controls.createHotkeys = function() {
   key2 = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
   key3 = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
   key4 = game.input.keyboard.addKey(Phaser.Keyboard.FOUR);
+
+  keys.push(key1, key2, key3, key4);
 }
