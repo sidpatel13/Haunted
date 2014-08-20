df15aab515afbfcb39705c2b358f5b42506f9385
Rootul <rootulp@gmail.com>
2014-08-20 15:17:24 -0400

    remove home controller and remove sandbox

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,18 +1,16 @@
 class RoomsController < ApplicationController
 
-  def generate
-    sample_pool = [*"a".."z"] + [*"0".."9"]
-    room_session = sample_pool.sample(6).join("")
-    redirect_to "/#{room_session}"
+  def index
   end
 
   def show
     @room_session = params[:room_session]
   end
 
-  def sandbox
-    @sandbox = true
-    render :show
+  def generate
+    sample_pool = [*"a".."z"] + [*"0".."9"]
+    room_session = sample_pool.sample(6).join("")
+    redirect_to "/#{room_session}"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
-  root 'home#index'
-  post '/', to: 'rooms#generate'
-  get '/rooms', to: 'rooms#generate'
+  root 'rooms#index'
+  post '/',                     to: 'rooms#generate'
+  get '/new',                to: 'rooms#generate'
   get '/:room_session', to: 'rooms#show'
-  get '/rooms/sandbox', to: 'rooms#sandbox'
 end
